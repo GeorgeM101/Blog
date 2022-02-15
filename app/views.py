48fecfaa74f734f9c608e1bb43f9c8bdb15d5f7b
GeorgeM101 <georgmboya@gmail.com>
2022-02-15 07:52:32 +0300

    added read file

--- app/views.py
+++ app/views.py
@@ -4,8 +4,8 @@ from flask_bcrypt import bcrypt
 from app import app, db
 from .models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-import os
-import secrets
+from werkzeug.security import generate_password_hash,check_password_hash
+
 
 
 
@@ -44,7 +44,7 @@ def register():
         return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        hashed_password = generate_password_hash(form.password.data)
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
@@ -60,7 +60,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
+        if user and check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('home'))
@@ -75,17 +75,7 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route("/account", methods=['GET', 'POST'])
+@app.route("/account")
 @login_required
 def account():
-    form = UpdateAccountForm()
-    if form.validate_on_submit():
-        current_user.username = form.username.data
-        current_user.email = form.email.data
-        db.session.commit()
-        flash('Your account has been updated!', 'success')
-        return redirect(url_for('account'))
-    elif request.method == 'GET':
-        form.username.data = current_user.username
-        form.email.data = current_user.email
-    return render_template('account.html', title='Account', form=form)
+    return render_template('account.html', title='Account')
