cc0cdc4261f59ae0e1879adf16d6c67d169689a1
GeorgeM101 <georgmboya@gmail.com>
2022-02-17 22:52:24 +0300

    added update post file

--- app/views.py
+++ app/views.py
@@ -97,24 +97,11 @@ def new_post():
     return render_template('new_post.html', title='Create a blog',form=form)
 
 
-@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@app.route("/post/<int:post_id>")
 @login_required
 def update_post(post_id):
     post = Post.query.get_or_404(post_id)
-    if post.author != current_user:
-        abort(403)
-    form = PostBlog()
-    if form.validate_on_submit():
-        post.title = form.title.data
-        post.content = form.content.data
-        db.session.commit()
-        flash('Your post has been updated!', 'success')
-        return redirect(url_for('post', post_id=post.id))
-    elif request.method == 'GET':
-        form.title.data = post.title
-        form.content.data = post.content
-    return render_template('create_post.html', title='Update Post',
-                           form=form, legend='Update Post')
+    return render_template('post.html', title=post.title, post=post )
 
 # @app.route('/new_user')
 # def new_user():
