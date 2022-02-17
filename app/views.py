7d67e52eb23601be74d953ba31b85369efd0186b
GeorgeM101 <georgmboya@gmail.com>
2022-02-17 14:02:27 +0300

    added post update

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, flash, redirect, request
+from flask import Flask, abort, render_template, url_for, flash, redirect, request
 from .forms import RegistrationForm, LoginForm, PostBlog
 from flask_bcrypt import bcrypt
 from app import app, db
@@ -47,10 +47,9 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
-        if user(user.password,form.password.data):
+        if (user.email == form.email.data):
             login_user(user, remember=form.remember.data)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
+            return redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -69,16 +68,37 @@ def account():
 
 
 @app.route("/post/new", methods=['GET','POST'])
+@login_required
 def new_post():
     form = PostBlog()
     if form.validate_on_submit():
         # post = Post(title=form.title.data, content=form.content.data)
-        new_blog = Post(title=form.title.data, content=form.content.data, user_id=1)
+        new_blog = Post(title=form.title.data, content=form.content.data, user_id=current_user.id)
         db.session.add(new_blog)
         db.session.commit()
         flash("Your post has been created!", 'success')
         return redirect(url_for('home'))
     return render_template('new_post.html', title='Create a blog',form=form)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostBlog()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
 
 # @app.route('/new_user')
 # def new_user():
