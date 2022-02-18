bc482ca0972c785b90b2fd6a813dff7d7f85ef09
GeorgeM101 <georgmboya@gmail.com>
2022-02-18 15:05:58 +0300

    set config

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://access:Access@localhost/blog'
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
